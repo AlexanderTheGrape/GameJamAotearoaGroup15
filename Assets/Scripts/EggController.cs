1f5a438c98d4fad158b5d51cba703c376400240d
Alex Monk <amonk446@gmail.com>
2022-05-21 20:40:59 +1200

    Updates to button

--- Assets/Scripts/EggController.cs
+++ Assets/Scripts/EggController.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Timers;
 
 public abstract class EggController : MonoBehaviour
 {
-    private enum hatchStatus
+    private enum HatchStatus
     {
         unactivated,
         incubating,
         hatched
     }
+    private HatchStatus hatchStatus;
     private int tempDegreesCelcius { get; set; }
     private List<int> temperatureThresholdsForTransformation { get; set; }
     private int nestType { get; set; } // Change this later as I get more details on it
@@ -33,9 +35,17 @@ public abstract class EggController : MonoBehaviour
     private static void SetTimer(int milliseconds)
     {
         // Create a timer with a ___ second interval
-        var aSetTimer = new System.Timers.Timer(milliseconds);
-        aSetTimer.AutoReset = true;
-        aSetTimer.Enabled = true;
+        var timer = new System.Timers.Timer(milliseconds);
+        timer.AutoReset = false;
+        timer.Enabled = true;
+
+        // The event to be triggered once the timer is done
+        //timer.Elapsed += 
+    }
+
+    private static void EggInitiatedEvent(object source, ElapsedEventArgs e)
+    {
+        hatchStatus = HatchStatus.incubating;
     }
 
     public void increaseEggTemperature(int temperatureIncreaseCelcius)
