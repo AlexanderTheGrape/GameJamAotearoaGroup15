1ee4d681766a68fe2ed56ee66220fcd91245eb49
DESKTOP-L89SVK3\amonk <amonk446@gmail.com>
2022-05-22 13:26:43 +1200

    Added reset button

--- Assets/Scripts/EggController.cs
+++ Assets/Scripts/EggController.cs
@@ -1,114 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Timers;
 
 public class EggController : MonoBehaviour
 {
-    private enum HatchStatus
-    {
-        unactivated,
-        incubating,
-        hatched,
-        done
-    }
-    private HatchStatus hatchStatus;
+    public GameObject egg1Prefab;
+    public GameObject egg2Prefab;
+    public GameObject egg3Prefab;
 
-    private int tempDegreesCelcius { get; set; }
-    private List<int> temperatureThresholdsForTransformation { get; set; }
+    private GameObject clone;
+    private Egg currentEgg;
 
-    private int nestType { get; set; } // Change this later as I get more details on it
-
-    private int eggIncubationPeriodMilliseconds { get; set; }
-
-    private Animator animator;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        animator = GetComponent<Animator>();
-        animator.SetBool("isHatching", false);
-
-        this.tempDegreesCelcius = 0;
+        InstantiateEgg();
     }
 
+    
     // Update is called once per frame
     void Update()
     {
-        if (animator.GetBool("isFinishedHatching") == true)
-        {
-            HatchEgg();
-            this.hatchStatus = HatchStatus.done;
-        }
-        if (this.hatchStatus == HatchStatus.hatched)
-        {
-            HatchEgg();
-            this.hatchStatus = HatchStatus.done;
-        }
+        
     }
 
-    private void SetTimer(int milliseconds)
+    void InstantiateEgg()
     {
-        // Create a timer with a ___ second interval
-        var timer = new System.Timers.Timer(5000);
-        timer.AutoReset = false;
-        timer.Enabled = true;
+        // Instantiate at position (0, 0, 0) and zero rotation.
+        clone = Instantiate(egg1Prefab, new Vector2(0, 0), Quaternion.identity);
 
-        // The event to be triggered once the timer is done
-        timer.Elapsed += new ElapsedEventHandler(EggHatchedEvent);
+        currentEgg = clone.GetComponent<Egg>();
     }
 
-    private void EggHatchedEvent(object source, ElapsedEventArgs e)
+    public void ResetEgg()
     {
-        this.hatchStatus = HatchStatus.hatched;
-    }
+        Destroy(currentEgg);
+        Destroy(clone);
 
-    private void HatchEgg()
-    {
-
-        //animator.SetBool("isHatching", false);
-
-        //// TODO REMOVE THIS
-        //this.animator.SetBool("isCritter1", true);
-        //this.animator.SetBool("isHatching", false);
-        switch (this.tempDegreesCelcius)
-        {
-            case 0:
-                animator.SetBool("isCritter1", true);
-                break;
-            case 1:
-                animator.SetBool("isCritter2", true);
-                break;
-            case 2:
-                animator.SetBool("isCritter3", true);
-                break;
-            default:
-                animator.SetBool("isFritter", true);
-                break;
-        }
+        Start();
     }
 
     public void EggIncubationStartedEvent()
     {
-        this.hatchStatus = HatchStatus.incubating;
-        animator.SetBool("isHatching", true);
-        animator.speed = 0.1f;
-
-        // Should be commented out in favour of animation-end timing
-        SetTimer(eggIncubationPeriodMilliseconds);
+        currentEgg.EggIncubationStartedEvent();
     }
 
-    public void IncreaseEggTemperature(int temperatureIncreaseCelcius)
+    public void IncreaseEggTemperature()
     {
-        this.tempDegreesCelcius += temperatureIncreaseCelcius;
+        currentEgg.IncreaseEggTemperatureByOne();
     }
 
-    public void IncreaseEggTemperatureByOne()
+    public void HatchEgg()
     {
-        this.tempDegreesCelcius += 1;
+        currentEgg.HatchEgg();
     }
 }
-
-
-
